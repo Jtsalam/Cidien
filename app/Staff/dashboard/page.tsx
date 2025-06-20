00a4d3fcb9099c05213c3b0eeb3c3349278b0aee
Plachard-JT <michealsalam06@gmail.com>
2025-06-20 11:06:38 -0600

    [Small update]

--- app/Staff/dashboard/page.tsx
+++ app/Staff/dashboard/page.tsx
@@ -7,9 +7,12 @@ export default function DevicePage() {
     <div className="min-h-screen font-sans bg-[#2b2b2b]  flex flex-col">
       <MainPanel />
       <div className="flex-1 flex items-center justify-center py-2">
-        <div className="text-center font-bold text-lg text-[#f8f0f0] bg-[#6d8a55] rounded-lg p-12 w-[500px] max-w-full shadow-md">
-          <p>Device</p>
-        </div>
+        <button>
+          <div className="text-center font-bold text-lg text-[#f8f0f0] bg-[#6d8a55] rounded-lg p-12 w-[500px] max-w-full shadow-md transition transform active:scale-95 duration-150 ease-in-out">
+            <p>Device</p>
+          </div>
+        </button>
+        
       </div>
     </div>
   );
