aad58b97267d8bd2a59ffc828cc46d1096ad7b1c
Plachard-JT <michealsalam06@gmail.com>
2025-06-26 05:11:12 -0600

    Changes to site access
    Managed access and flow of certain pages:
    > Staff dashboard can't be accessed without signing in
    > User get's redirected to Staff dashboard if they've already signed in:
    this means they will not be able to enter the first two pages again

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const orgSubmitted = request.cookies.get("orgSubmitted")?.value;
+  const staffSubmitted = request.cookies.get("staffSubmitted")?.value;
+  const userRole = request.cookies.get("user_role")?.value;
   const pathname = request.nextUrl.pathname;
 
   // If user hasn't submitted Center, redirect them back to the sign-in page
@@ -14,11 +16,29 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/Staff/sign-in", request.url));
   }
 
+  // If user hasn't signed in with Staff sign in, redirect them back to sign-in page
+  if (pathname.startsWith(`/${userRole}/dashboard`) && !staffSubmitted) {
+    return NextResponse.redirect(new URL("/Staff/sign-in", request.url));
+  }
+  
+  // // Block access to staff dashboard if not staff
+  // if (pathname.startsWith("/Staff/dashboard") && userRole !== "Staff") {
+  //   return NextResponse.redirect(new URL(`/${userRole}/dashboard`, request.url));
+  // }
+
+  if (pathname === "/Staff/sign-in" && staffSubmitted) {
+    return NextResponse.redirect(new URL(`/${userRole}/dashboard`, request.url));
+  }
+
   // Allow access to the page requested for if the user has submitted
   return NextResponse.next();
 }
 
 // Apply the middleware to the following pages
 export const config = {
-  matcher: ["/Center/sign-in", "/Staff/sign-in"]  // Change this to the actual route of your new page
+  matcher: ["/Center/sign-in",
+           "/Staff/sign-in", 
+           "/Staff/dashboard",
+          "/Admin/dashboard",
+        ]  // Change this to the actual route of your new page
 };
