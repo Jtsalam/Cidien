c81515532da7f248c887365227c8ec5e79416363
Plachard-JT <michealsalam06@gmail.com>
2025-06-26 05:07:48 -0600

    minor changes to cookie modification

--- app/api/center/logout/route.ts
+++ app/api/center/logout/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   const response = NextResponse.json({ message: "Logged out successfully" });
-  response.headers.set("Set-Cookie", "orgSubmitted=; Path=/; HttpOnly; Max-Age=0"); // Expire the cookie
+  response.headers.append("Set-Cookie", "orgSubmitted=; Path=/; HttpOnly; Max-Age=0"); // Expire the cookie
   return response;
 }
