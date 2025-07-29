cba0a8dd8c5adcab83a04fb5f0d8ce4ee6eb8788
Plachard-JT <michealsalam06@gmail.com>
2025-07-28 19:11:53 -0600

    Paused Inactivity tracker
    Paused Inactivity tracker for both dashboards: currently feel this might not be necessary as the whole point of charting is to see important info in real-time

--- app/(Admin)/dashboard/AdminDashboard.tsx
+++ app/(Admin)/dashboard/AdminDashboard.tsx
@@ -6,7 +6,7 @@ export default function RoomDashboard() {
     return (
         <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
             <AdminPanel />
-            <InactivityTracker />
+            {/* <InactivityTracker /> */}
         </div>
     );
 }
--- app/(Staff)/dashboard/StaffDashboard.tsx
+++ app/(Staff)/dashboard/StaffDashboard.tsx
@@ -7,7 +7,7 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
       <MainPanel />
-      <InactivityTracker />
+      {/* <InactivityTracker /> */}
     </div>
   )
 }
