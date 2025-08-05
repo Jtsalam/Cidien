be3643a9cd787ce4c7571d75e1969eda2136ea9e
Plachard-JT <michealsalam06@gmail.com>
2025-08-04 18:59:25 -0600

    Added Error boundary to staff dashboard

--- app/(Staff)/dashboard/StaffDashboard.tsx
+++ app/(Staff)/dashboard/StaffDashboard.tsx
@@ -2,12 +2,15 @@
 
 import MainPanel from "@/components/Staff/Dashboard/MainPanel";
 import InactivityTracker from "@/components/InactivityTracker";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function Dashboard() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
-      <MainPanel />
-      {/* <InactivityTracker /> */}
-    </div>
+    <ErrorBoundary>
+      <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+        <MainPanel />
+        {/* <InactivityTracker /> */}
+      </div>
+    </ErrorBoundary>
   )
 }
